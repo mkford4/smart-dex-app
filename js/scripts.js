e3fc5a95a43279b7fbd5edf63e214bf2a785e6ca
Monika <91907563+mkford4@users.noreply.github.com>
2021-11-02 12:28:00 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// Comments go here //
+let myName = 'Monika';
+document.write(myName);
+
+/* myName =  'Jessika';
+document.write(myName);*/
+
+/*let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);*/
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size/2);
+document.write(minSize);
