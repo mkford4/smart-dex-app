fb2f82e12f0ec37ce91b7482a258c87ddcd5ef55
Monika <91907563+mkford4@users.noreply.github.com>
2021-11-02 12:37:15 -0500

    Added comments with examples & favoriteFood to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,25 @@
-// Comments go here //
+alert('Hello World');
+
+let favoriteFood = 'Enchiladas';
+document.write(favoriteFood);
+
+
+/* //myName example & re-assigning variable://
 let myName = 'Monika';
 document.write(myName);
 
-/* myName =  'Jessika';
+myName =  'Jessika';
 document.write(myName);*/
 
-/*let simpleAddition = 2 + 2;
+/* //Addition & Multiplication examples//
+let simpleAddition = 2 + 2;
 document.write(simpleAddition);
 
 let size = 100;
 let doubleSize = size * 2;
-document.write(doubleSize);*/
+document.write(doubleSize);
 
 let size = 100;
 let doubleSize = size * 2;
 let minSize = (doubleSize * 2) - (size/2);
-document.write(minSize);
+document.write(minSize); */
