a1a7cea5b194eaec20843ec58b527ac136ef392d
Monika <91907563+mkford4@users.noreply.github.com>
2021-11-03 15:21:44 -0500

    Added pokemonList array with 4 pokemon objects

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+pokemonList = [
+  {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+  {name: 'Charmander', height: 0.6, types: 'fire'},
+  {name: 'Squirtle', height: 0.5, types: 'water'},
+  {name: 'Pikachu', height: 0.4, types: 'electric'},
+]
