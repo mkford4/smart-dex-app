c86c513c2f65e5cf81a0d930aad12ee23548f227
Monika <91907563+mkford4@users.noreply.github.com>
2021-11-05 14:14:46 -0500

    Added for loop & conditional to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,30 @@ pokemonList = [
   {name: 'Pikachu', height: 1.04, types: 'electric'},
   {name: 'Sandslash', height: 3.3, types: 'ground'},
 ]
+
+// for loop: Lists out pokemon in pokemonList with height; added conditional for tallest Pokemon with extra message
+for (i=0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 4) {
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + ' - Wow, that\'s big!<p>')
+  } else {
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')<p>')
+  }
+}
+
+/* for loop example 1.3
+let personAge = [
+  {name: 'person1', age: 16},
+  {name: 'person2', age: 10},
+  {name: 'person3', age: 25}
+];
+
+for  (let i=0; i < personAge.length; i++) {
+  if (personAge[i].age < 19 && personAge[i].age > 13){
+    console.log(personAge[i].name + ' is a teenager')
+  } else if (personAge[i].age < 13) {
+    console.log(personAge[i].name +' is a child')
+  } else {
+    console.log(personAge[i].name + ' is an adult')
+  }
+}
+*/
