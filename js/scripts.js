f296e1e9979c9592dbcdcb9bfb956cee19547409
Monika <91907563+mkford4@users.noreply.github.com>
2021-11-05 14:13:48 -0500

    Added to pokemonList and adjusted height values

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 pokemonList = [
-  {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-  {name: 'Charmander', height: 0.6, types: 'fire'},
-  {name: 'Squirtle', height: 0.5, types: 'water'},
-  {name: 'Pikachu', height: 0.4, types: 'electric'},
+  {name: 'Bulbasaur', height: 2.04, types: ['grass', 'poison']},
+  {name: 'Charmander', height: 2, types: 'fire'},
+  {name: 'Squirtle', height: 1.08, types: 'water'},
+  {name: 'Alakazam', height: 4.11, types: 'psychic'},
+  {name: 'Pikachu', height: 1.04, types: 'electric'},
+  {name: 'Sandslash', height: 3.3, types: 'ground'},
 ]
