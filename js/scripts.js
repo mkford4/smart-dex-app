72bbb007cacf396d4cf95ab334361c52fffacfc7
Monika <91907563+mkford4@users.noreply.github.com>
2021-11-10 15:26:56 -0600

    Added forEach loop to scripts.js
    pokemonList.forEach...

--- js/scripts.js
+++ js/scripts.js
@@ -13,14 +13,24 @@ let pokemonList2 = [
   {name: 'Sandslash', height: 3.3, types: 'ground'},
 ]
 
-// for loop: Lists out pokemon in pokemonList with height; added conditional for tallest Pokemon with extra message
+// forEach loop (simplified from for loop below)
+pokemonList.forEach( function(pokemon) {
+  if (pokemon.height > 4) {
+    document.write( '<p>' + pokemon.name + ' (height: ' + pokemon.height + ') - Wow, that\'s big! </p>' )
+  } else {
+    document.write( '<p>' + pokemon.name + ' (height: ' + pokemon.height + ') </p>')
+  }
+});
+
+/* for loop: Lists out pokemon in pokemonList with height; added conditional for tallest Pokemon with extra message
 for (i=0; i < pokemonList.length; i++) {
   if (pokemonList[i].height > 4) {
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + ' - Wow, that\'s big!<p>')
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s big!<p>')
   } else {
     document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')<p>')
   }
 }
+*/
 
 // printArrayDetails function loop example with parameter (list)
 /* function printArrayDetails(list) {
@@ -32,22 +42,4 @@ for (i=0; i < pokemonList.length; i++) {
 
 printArrayDetails(pokemonList);
 printArrayDetails(pokemonList2);
-*/
-
-/* for loop example 1.3
-let personAge = [
-  {name: 'person1', age: 16},
-  {name: 'person2', age: 10},
-  {name: 'person3', age: 25}
-];
-
-for  (let i=0; i < personAge.length; i++) {
-  if (personAge[i].age < 19 && personAge[i].age > 13){
-    console.log(personAge[i].name + ' is a teenager')
-  } else if (personAge[i].age < 13) {
-    console.log(personAge[i].name +' is a child')
-  } else {
-    console.log(personAge[i].name + ' is an adult')
-  }
-}
 */
